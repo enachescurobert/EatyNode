f77dce950ac852b16fdda93b718051c50a1ab523
Enachescu <enachescurobert@gmail.com>
2018-12-24 19:00:02 +0200

    Like for wishlist, + for session

--- api/models/product.js
+++ api/models/product.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: false },
-    price: { type: Number, required: false },
+    price: { type: Number, required: false, default:0},
     like: { type: Number, required: false , default:0},
     quantity: { type: Number, required: false, default:0 },
 
